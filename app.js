16cd6ad00fd51f2b2b0045c80f9fa2a8c8828e70
Hugo Urquiza <hugo.urquiza@gmail.com>
2022-03-24 11:30:52 -0300

    added server/status

--- app.js
+++ app.js
@@ -2,11 +2,13 @@ const express = require('express')
 const port = 3000
 const wallet_router = require('./lib/wallet')
 const anchor_router = require('./lib/anchor')
+const server_router = require('./lib/server')
 const app = express()
 
 app.use(express.json());
 
 app.use('/wallet', wallet_router);
 app.use('/anchor', anchor_router);
+app.use('/server',server_router);
 
 module.exports = app;
