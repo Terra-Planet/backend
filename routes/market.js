fa10487bba30f92855e608725cf15e93704ad7f2
Hugo Urquiza <hugo.urquiza@gmail.com>
2022-04-06 08:50:55 -0300

    fixed undefined terra ref

--- routes/market.js
+++ routes/market.js
@@ -4,7 +4,9 @@ const router = express.Router();
 const utils = require('../lib/utils');
 
 router.get('/rate/:token_in/:token_out', function(req, res) {
-    utils.terra.market.swapRate(new Coin(req.params.token_in,1),req.params.token_out).then ( (r)=> {        
+    const terra=utils.get_lcd_client('main');
+
+    terra.market.swapRate(new Coin(req.params.token_in,1),req.params.token_out).then ( (r)=> {        
         res.send({token:r.denom, amount:r.amount});
     }).catch ( (err) => {
         res.status(400).send({status:'err',msg:err.message});
