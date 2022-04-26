4da74c91dd8cb617d43e0ce88ea0d24c4f0899d4
Ezequiel (Kimi) Aceto <ezequiel.aceto@gmail.com>
2022-04-26 11:59:47 +0200

    Added shutdown endpoint

--- routes/server.js
+++ routes/server.js
@@ -2,7 +2,11 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/status', async (req, res, next) =>{    
-    res.send({status:'ok'})    
+    res.send({status:'ok'})
+});
+
+router.get('/shutdown', async (req, res, next) =>{    
+    process.exit(0)
 });
 
 module.exports = router;
