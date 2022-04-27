9b16c680cc8b08d963f679a0b0ac69a3700d30fb
Ezequiel Aceto <eaceto@users.noreply.github.com>
2022-04-27 14:23:52 +0200

    Removed unused route

--- routes/server.js
+++ routes/server.js
@@ -5,8 +5,4 @@ router.get('/status', async (req, res, next) =>{
     res.send({status:'ok'})
 });
 
-router.get('/shutdown', async (req, res, next) =>{    
-    process.exit(0)
-});
-
 module.exports = router;
