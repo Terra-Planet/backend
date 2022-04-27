9fb6e254ee304c185df059e5ea07f172dd17906b
f0go <f0go.dev@gmail.com>
2022-04-27 10:45:42 -0300

    fix script typo

--- scripts/clean_for_build.sh
+++ scripts/clean_for_build.sh
@@ -3,6 +3,6 @@
 # Removes files that App Store doesn't like
 #
 
-for i in `find . -name "prebuilds”`; do rm -rf $i; done 
-for i in `find . -name "Release`; do rm -rf $i; done 
+for i in `find . -name "prebuilds"`; do rm -rf $i; done 
+for i in `find . -name "Release"`; do rm -rf $i; done 
 
